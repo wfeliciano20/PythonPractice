d77988877350102e63563fc15c097b7e57776c72
william feliciano <williamfeliciano@williams-MacBook-Pro.local>
2020-04-10 00:05:14 -0400

    intro to python files and vs code settings file

---
+++ IntroToPython/numbers.py
@@ -0,0 +1,21 @@
+age = 35  # Integer
+PI = 3.14159  # Float
+
+maths_operation = 1 + 3 * 4 / 2 - 2
+print(maths_operation)
+
+# float division
+float_division = 12 / 3
+print(float_division)
+
+# integer division
+integer_division = 12 // 3
+print(integer_division)
+
+# remainder
+remainder = 13 % 5
+print(remainder)
+
+# odd or even using modulus
+even = 10 % 2
+odd = 3 % 2
---
+++ IntroToPython/variablesAndPrinting.py
@@ -0,0 +1,7 @@
+# variables
+age = 30
+# printing
+print(age)
+# constants
+PI = 3.14159
+RADIANS_TO_DEGREES = 180/PI
