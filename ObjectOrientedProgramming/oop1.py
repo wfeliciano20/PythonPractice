5b52fd52f20ef2944cd8a31fe7c9f5339b1da168
william feliciano <williamfeliciano@williams-MacBook-Pro.local>
2020-04-13 18:37:35 -0400

    oop1 file

---
+++ ObjectOrientedProgramming/oop1.py
@@ -0,0 +1,22 @@
+my_student = {
+    'name': 'Rolf Smith',
+    'grades': [70, 88, 90, 99]
+}
+
+
+def average(student):
+    return sum(student['grades']) / len(student['grades'])
+
+
+class Student:
+    def __init__(self, new_name, new_grades):
+        self.name = new_name
+        self.grades = new_grades
+
+    def average(self):
+        return sum(self.grades) / len(self.grades)
+
+
+student_one = Student('Rolf Smith', [70, 88, 90, 99])
+student_two = Student('Jose', [50, 60, 99, 100])
+print(student_one.average())
